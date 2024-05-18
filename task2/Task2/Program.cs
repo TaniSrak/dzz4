edcd3f76f6c238e0ca07cb5acb2d94bf1feaff84
Татьяна Кожевникова <tatanakozevnikova786@gmail.com>
2024-05-18 15:40:53 +0500

    добавила вторую задачу

--- task2/Task2/Program.cs
+++ task2/Task2/Program.cs
@@ -4,7 +4,82 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Kettle kettle = new Kettle("Чайник Philips", "Чайник для кипячения воды, 1.7L, 2000W");
+            Microwave microwave = new Microwave("Микроволновка LG", "Микроволновая печь, 700W, 20L");
+            Car car = new Car("Автомобиль Tesla", "Электромобиль, 300kW, 600km range");
+            Steamship steamship = new Steamship("Пароход Titanic", "Пассажирский пароход, длина 268.98 м, ширина –– 28.2 м");
+
+            Device[] devices = { kettle, microwave, car, steamship };
+
+            foreach (var device in devices)
+            {
+                device.Show();
+                device.Desc();
+                device.Sound();
+                Console.WriteLine();
+            }
+        }
+    }
+
+    public abstract class Device
+    {
+        public string Name { get; set; }
+        public string Characteristics { get; set; }
+
+        public Device(string name, string characteristics)
+        {
+            Name = name;
+            Characteristics = characteristics;
+        }
+
+        public abstract void Sound();
+        public void Show()
+        {
+            Console.WriteLine($"Устройство: {Name}");
+        }
+        public void Desc()
+        {
+            Console.WriteLine($"Описание: {Characteristics}");
+        }
+    }
+    //чайник
+    public class Kettle : Device
+    {
+        public Kettle(string name, string characteristics) : base(name, characteristics) { }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Чайник: Шипит\n");
+        }
+    }
+    //микроволновка
+    public class Microwave : Device
+    {
+        public Microwave(string name, string characteristics) : base(name, characteristics) { }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Микроволновка: Пищит");
+        }
+    }
+    //автомобиль
+    public class Car : Device
+    {
+        public Car(string name, string characteristics) : base(name, characteristics) { }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Автомобиль: рычит");
+        }
+    }
+    //пароход
+    public class Steamship : Device
+    {
+        public Steamship(string name, string characteristics) : base(name, characteristics) { }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Пароход: Гудит");
         }
     }
 }
--- task2/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ task2/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4018b29130f571e1dfcacbe9ee5dcd807627c741")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d6b2954d9c5656703b1541656a4b5a652690e7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
